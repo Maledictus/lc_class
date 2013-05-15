dfaf76fecdb932a14c8b2cbb1e6781cb74326b48
Oleg Linkin <MaledictusDeMagog@gmail.com>
2013-05-15 19:15:44 +0300

    initial project import from KDevelop

---
+++ lc_class/class.h
@@ -0,0 +1,13 @@
+{% extends "cpp_header.h" %} 
+
+{% block class_declaration_open %}
+/**
+ * This is an example class template. 
+ * It showcases some of the features of Grantlee templates
+ */
+{{ block.super }}
+{% endblock class_declaration_open %}
+
+{% block class_bottom %}
+// Feel free to add extra class memmbers
+{% endblock class_bottom %}
